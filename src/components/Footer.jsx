7d099c405dfb1727bc5a6acc06f2371cd246472a
DarkuBaby <143433855+DarkuBaby@users.noreply.github.com>
2025-06-06 06:09:57 +0545

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-[#0f172a] text-gray-300 text-sm text-center py-8">
-        <p>&copy; 2024 Vapour. All rights reserved.</p>
+        <p>&copy; 2025 GamerBerg. All rights reserved.</p>
       </footer>
   )
 }
